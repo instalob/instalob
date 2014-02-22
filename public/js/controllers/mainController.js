be2da3709092ee0c919e96c2b44616cc0238d117
sudostack <dwu207@gmail.com>
2014-02-21 20:32:57 -0800

    created a main view with a hashtag submit form

--- public/js/controllers/mainController.js
+++ public/js/controllers/mainController.js
@@ -1,5 +1,11 @@
 angular.module('myApp.controllers').
 
 controller("MainController", ['$scope', function($scope) {
+
   $scope.title = "Main hey hey!";
+
+  $scope.hashtagSubmit = function() {
+    console.log('Submitting hashtags');
+  };
+
 }]);
